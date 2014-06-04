4a0dc59fae79d8b6b4be84c45bc76074f75792f7
Jeff Haynie <jhaynie@appcelerator.com>
2014-06-04 01:48:20 -0700

    symlink simulator->emulator

--- index.js
+++ index.js
@@ -5,5 +5,7 @@
 module.exports = exports = {
 	device: require('./lib/device'),
 	simulator: require('./lib/simulator'),
+	// symlink
+	emulator: require('./lib/simulator'),
 	env: require('./lib/env')
 };
